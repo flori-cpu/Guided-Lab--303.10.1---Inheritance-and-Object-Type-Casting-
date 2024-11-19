74cc33424ce5482c2afb396b4b48a84dcabc260a
flori-cpu <flori.jasienowski@outlook.com>
2024-11-19 12:50:23 -0500

    first commit

---
+++ Surgeon.java
@@ -0,0 +1,7 @@
+public class Surgeon extends Doctor { 
+    void Surgeon_Details() {
+        Department = "Cardio"; 
+        System.out.println("Surgeon Detail...");
+        System.out.println("cardio " );
+    }
+}
